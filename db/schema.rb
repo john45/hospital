bae30e74d184d52ef6bf7561015bfaf4511c99e9
Adamenko <mangamen45@gmail.com>
2017-06-04 00:30:26 +0300

    add statics controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170603204654) do
+ActiveRecord::Schema.define(version: 20170603211504) do
 
   create_table "avatars", force: :cascade do |t|
     t.string   "name"
@@ -42,6 +42,13 @@ ActiveRecord::Schema.define(version: 20170603204654) do
     t.integer  "insurance_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "records", force: :cascade do |t|
